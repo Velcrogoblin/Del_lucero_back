13108cd028e236f92a900892beb1ed6d2b7b8685
unknown <ddandolo@yahoo.com>
2023-09-28 13:18:41 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const server = require("./src/app.js");
+const cors = require("cors");
+const app = express();
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
+
+app.use(cors({
+  origin: "https://dellucero.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}))
+
+
 
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
