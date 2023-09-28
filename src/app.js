eaa589db550a7d085387cfca27d54a3ca1dcd4a3
unknown <ddandolo@yahoo.com>
2023-09-28 13:35:30 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require("./db.js");
 const server = express();
 
 server.use(cors({
-  origin: "https://dellucero.vercel.app/",
+  origin: "https://dellucero.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 
@@ -22,7 +22,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://dellucero.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://dellucero.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
